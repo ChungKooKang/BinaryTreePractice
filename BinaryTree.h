c41b0c3c2baa78391da2e461c4a8950fb2efe914
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-06-23 17:01:10 +0900

    초기 커밋.

---
+++ BinaryTree.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <iostream>
+
+namespace myTree
+{
+	struct Node
+	{
+		int mData;
+
+		Node* mpLeft;
+		Node* mpRight;
+
+		Node(int data, Node* left, Node* right)
+		{
+			mData = data;
+			mpLeft = left;
+			mpRight = right;
+		}
+	};
+
+	class BinaryTree
+	{
+	private :
+		Node* mpRoot{};
+
+	public :
+		BinaryTree()
+		{
+			mpRoot = CreateNode(0);
+		}
+
+		Node* GetRoot() const
+		{
+			return mpRoot;
+		}
+
+		Node* CreateNode(int data)
+		{
+			new Node(data, nullptr, nullptr);
+		}
+
+		Node* InsertLeft(Node* pParent, int data)
+		{
+			pParent->mpLeft = CreateNode(data);
+
+			return pParent->mpLeft;
+		}
+
+		Node* InsertRight(Node* pParent, int data)
+		{
+			pParent->mpRight = CreateNode(data);
+
+			return pParent->mpRight;
+		}
+	};
+
+}
+
---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+#include "BinaryTree.h"
+int main()
+{
+	myTree::BinaryTree myTree;
+
+	auto pRoot = myTree.GetRoot();
+	pRoot->mData = 1;
+
+	auto pNode = myTree.InsertLeft(pRoot, 2);
+}
+
