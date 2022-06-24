bf98db2b9eb9b0cf0050c94fd763b631e6cf84c7
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-06-24 15:11:56 +0900

    예제까지 Tree 완성!

--- main.cpp
+++ main.cpp
@@ -9,5 +9,11 @@ int main()
 	pRoot->mData = 1;
 
 	auto pNode = myTree.InsertLeft(pRoot, 2);
+	myTree.InsertLeft(pNode, 4);
+	myTree.InsertRight(pNode, 5);
+
+	pNode =	myTree.InsertRight(pRoot, 3);
+	myTree.InsertLeft(pNode, 6);
+	myTree.InsertRight(pNode, 7);
 }
 
