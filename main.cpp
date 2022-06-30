fe7c937c3ba1562c33c0e6fb26a011bc684db327
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-06-30 16:17:21 +0900

    heap 정렬 완료!

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-	QuadTree qt1(Point(0,0), Point(8, 8));
+	QuadTree qt1(Point(0, 0), Point(8, 8));
 
 	std::unique_ptr<Node> a = std::make_unique<Node>(Point(1, 1), 1);
 	std::unique_ptr<Node> b = std::make_unique<Node>(Point(2, 2), 2);
@@ -27,4 +27,36 @@ int main()
 		std::cout << "Not Found" << std::endl;
 	}
 
+	std::vector<int> v{ 1, 2, 3, 4, 5 };
+
+	// C++20
+	std::make_heap(v.begin(), v.end());
+
+	for (auto& e : v)
+	{
+		std::cout << e << std::endl;
+	}
+
+	std::cout << "-------------" << std::endl;
+	// 정렬
+
+	while (!v.empty())
+	{
+		std::cout << v.front() << std::endl;
+		std::pop_heap(v.begin(), v.end());
+		v.pop_back();
+	}
+
+
+	// min heap
+
+	v = { 4, 2, 5, 1, 3 };
+	std::make_heap(v.begin(), v.end(), std::greater<>());
+
+	std::cout << "-------------" << std::endl;
+
+	for (auto& e : v)
+	{
+		std::cout << e << std::endl;
+	}
 }
